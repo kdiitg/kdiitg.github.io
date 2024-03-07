cede82fe2ebd27071f9ac8a30c61cb9e34a24596
Kuldeep Singh <kdiitg@gmail.com>
2024-03-07 15:54:05 +0530

    messege counter

--- index.js
+++ index.js
@@ -14,3 +14,21 @@ function handleClick() {
 
 // Add an event listener to the button, listening for the 'click' event
 button.addEventListener('click', handleClick);
+
+function incrementVisitCount() {
+    if (typeof(Storage) !== "undefined") {
+      if (localStorage.visitCount) {
+        localStorage.visitCount = Number(localStorage.visitCount) + 1;
+      } else {
+        localStorage.visitCount = 1;
+      }
+      document.getElementById("visitCount").innerHTML = "You have visited this page " + localStorage.visitCount + " time(s).";
+    } else {
+      document.getElementById("visitCount").innerHTML = "Sorry, your browser does not support web storage...";
+    }
+  }
+
+  // Call the function when the page loads
+  window.onload = function() {
+    incrementVisitCount();
+  };
